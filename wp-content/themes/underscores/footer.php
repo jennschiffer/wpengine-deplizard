5815d292764dfff922575176cd8524e46c7904d3
Jenn Schiffer <jenn@dotbiz.info>
2015-10-22 17:14:20 -0400

    de-brand underscores

--- wp-content/themes/underscores/footer.php
+++ wp-content/themes/underscores/footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'underscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'underscores' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="http://bocoup.com" rel="designer">Bocoup</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'underscores' ), 'underscores', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
